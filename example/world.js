7a854e565b34691886d9b26f5b9bb2da82a4a443
Kyle Robinson Young <kyle@dontkry.com>
2014-02-09 19:57:48 -0800

    Update main example

--- example/world.js
+++ example/world.js
@@ -2,18 +2,32 @@ var createGame = require('voxel-engine');
 var createTerrain = require('voxel-perlin-terrain');
 
 var game = createGame({
-  generateVoxelChunk: createTerrain({scaleFactor:6}),
+  generateChunks: false,
   chunkDistance: 2,
-  materials: ['brick', ['grass', 'dirt', 'grass_dirt'], 'plank', 'obsidian', 'redwool', 'bluewool'],
+  materials: [['grass', 'dirt', 'grass_dirt'], 'brick', 'plank', 'obsidian', 'redwool', 'bluewool'],
   texturePath: 'textures/'
 });
 var container = document.body;
 game.appendTo(container);
 
+// create the terrain
+var terrain = createTerrain('virus', 0, 5);
+game.voxels.on('missingChunk', function(p) {
+  var voxels = terrain(p, 32);
+  var chunk = {
+    position: p,
+    dims: [32, 32, 32],
+    voxels: voxels
+  };
+  game.showChunk(chunk);
+});
+
+game.paused = false;
+
 // create a player
 var createPlayer = require('voxel-player')(game);
 var shama = createPlayer('textures/shama.png');
-shama.yaw.position.set(0, 10, 0);
+shama.yaw.position.set(0, 15, 0);
 shama.possess();
 
 // create the virus
